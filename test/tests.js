883eb7b007e59f224316f06a68fc475209d5806a
Mallarie <mallariemilano@gmail.com>
2019-02-03 17:08:11 -0800

    fixed project errors and created a new repo because I believe there is an issue with the previous

---
+++ test/tests.js
@@ -0,0 +1,11 @@
+const test = QUnit.test;
+import headsTails from '../src/heads-tails.js';
+
+test('if random number less than 0.5, return heads', function(assert){
+    const result = headsTails(0.2);
+    assert.equal(result, 'heads');
+});
+test('if random number great than 0.5, return tails', function(assert){
+    const result = headsTails(0.8);
+    assert.equal(result, 'tails');
+});
